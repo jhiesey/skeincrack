0bdf946f0ceb051d7390cf6d16a5ee44f89c7143
Justin Lebar <justin.lebar@gmail.com>
2013-04-01 16:03:41 -0400

    Add hacky benchmark mode.
    Pass --benchmark to get hashes/sec for one thread.

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ CFLAGS = -O3 -Wall -ICD/Optimized_64bit --std=c99 -lpthread -fno-strict-aliasing
 
 all: skeincrack
 
-skeincrack: skeincrack.c
+skeincrack: Makefile skeincrack.c
 	$(CC) $(CFLAGS) CD/Optimized_64bit/*.c skeincrack.c -o $@
 
 .PHONY: clean
--- skeincrack.c
+++ skeincrack.c
@@ -1,6 +1,7 @@
 #include "SHA3api_ref.h"
 #include "skein.h"
 #include <sys/types.h>
+#include <sys/time.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdbool.h>
@@ -13,8 +14,11 @@
 
 #define BUF_SIZE 128
 #define HASH_BITS 1024
+static const int HASHES_BEFORE_BENCHMARK = 10000;
 
 static const char *param = "hashable=";
+
+static bool benchmarkMode = false;
 
 static const unsigned char target[HASH_BITS/8] = {0x5b, 0x4d, 0xa9, 0x5f, 0x5f, 0xa0, 0x82, 0x80, 0xfc, 0x98, 0x79, 0xdf, 0x44, 0xf4, 0x18, 0xc8, 0xf9, 0xf1, 0x2b, 0xa4, 0x24, 0xb7, 0x75, 0x7d, 0xe0, 0x2b, 0xbd, 0xfb, 0xae, 0x0d, 0x4c, 0x4f, 0xdf, 0x93, 0x17, 0xc8, 0x0c, 0xc5, 0xfe, 0x04, 0xc6, 0x42, 0x90, 0x73, 0x46, 0x6c, 0xf2, 0x97, 0x06, 0xb8, 0xc2, 0x59, 0x99, 0xdd, 0xd2, 0xf6, 0x54, 0x0d, 0x44, 0x75, 0xcc, 0x97, 0x7b, 0x87, 0xf4, 0x75, 0x7b, 0xe0, 0x23, 0xf1, 0x9b, 0x8f, 0x40, 0x35, 0xd7, 0x72, 0x28, 0x86, 0xb7, 0x88, 0x69, 0x82, 0x6d, 0xe9, 0x16, 0xa7, 0x9c, 0xf9, 0xc9, 0x4c, 0xc7, 0x9c, 0xd4, 0x34, 0x7d, 0x24, 0xb5, 0x67, 0xaa, 0x3e, 0x23, 0x90, 0xa5, 0x73, 0xa3, 0x73, 0xa4, 0x8a, 0x5e, 0x67, 0x66, 0x40, 0xc7, 0x9c, 0xc7, 0x01, 0x97, 0xe1, 0xc5, 0xe7, 0xf9, 0x02, 0xfb, 0x53, 0xca, 0x18, 0x58, 0xb6};
 
@@ -103,13 +107,30 @@ void *hashThread(void *aux) {
   read(randfd, buf, BUF_SIZE);
   normalizeBuffer(buf, BUF_SIZE);
 
+  int numHashes = 0;
+  struct timeval startTimestamp;
+  gettimeofday(&startTimestamp, NULL);
+
   while(true) {
     incrementArbitraryPrecision(buf, BUF_SIZE);
     unsigned char hash[HASH_BITS / CHAR_BIT];
     Hash(HASH_BITS, buf, BUF_SIZE * CHAR_BIT, hash);
 
+    numHashes++;
+    if(benchmarkMode && numHashes == HASHES_BEFORE_BENCHMARK) {
+      struct timeval timestamp;
+      gettimeofday(&timestamp, NULL);
+
+      double sec = (timestamp.tv_sec - startTimestamp.tv_sec) +
+                   (timestamp.tv_usec - startTimestamp.tv_usec) / 1000000.0;
+      printf("Thread %d: %0.f hashes/sec (%d hashes in %.5fs)\n",
+             threadNum, HASHES_BEFORE_BENCHMARK / sec,
+             HASHES_BEFORE_BENCHMARK, sec);
+      return;
+    }
+
     int distance = hammingDistance(target, hash, HASH_BITS/8);
-    if (distance < threadBest) {
+    if (!benchmarkMode && distance < threadBest) {
       threadBest = distance;
 
       // check if this is actually the best over all threads
@@ -125,6 +146,11 @@ void *hashThread(void *aux) {
       best = distance;
 
       printf("Data: %s\n", buf);
+
+      // Only invoke curl if we're not in benchmark mode.
+      if (benchmarkMode) {
+        continue;
+      }
 
       int outfd = open("data.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
       if(outfd < 0)
@@ -144,10 +170,19 @@ void *hashThread(void *aux) {
 }
 
 
-int main(void) {
+int main(int argc, char** argv) {
   pthread_mutex_init(&bestLock, NULL);
 
-  long numCPUs = sysconf(_SC_NPROCESSORS_ONLN); // Get number of cores
+  if (argc == 2 && !strcmp("--benchmark", argv[1])) {
+    benchmarkMode = true;
+  }
+
+  long numCPUs;
+  if (!benchmarkMode) {
+    numCPUs = sysconf(_SC_NPROCESSORS_ONLN); // Get number of cores
+  } else {
+    numCPUs = 1;
+  }
 
   pthread_t *threads = malloc(sizeof(pthread_t) * numCPUs);
   int *threadNums = malloc(sizeof(int) * numCPUs);
@@ -159,7 +194,8 @@ int main(void) {
       perror("Child thread");
   }
 
-  // Join threads. Since they never terminate, this will just block, which is fine
+  // Join threads. In non-benchmark mode, they never terminate, which means
+  // this will just block.
   for(int i = 0; i < numCPUs; i++) {
     int err = pthread_join(threads[i], NULL);
     if(err < 0)
