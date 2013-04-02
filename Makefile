6c28d37c9236a843e799e9302e6404a7a3f9f247
Justin Lebar <justin.lebar@gmail.com>
2013-04-02 00:06:42 -0400

    Add a |make pgo| target.  It's ~5% faster on my machine.
    Before:
    
      $ ./skeincrack --benchmark
      Thread 0 started
      Thread 0: 1363503 hashes/sec (1000000 hashes in 0.73340s)
      Thread 0: 1364411 hashes/sec (2000000 hashes in 1.46583s)
      Thread 0: 1369817 hashes/sec (3000000 hashes in 2.19007s)
    
    After:
    
      $ ./skeincrack --benchmark
      Thread 0 started
      Thread 0: 1442090 hashes/sec (1000000 hashes in 0.69344s)
      Thread 0: 1444450 hashes/sec (2000000 hashes in 1.38461s)
      Thread 0: 1446436 hashes/sec (3000000 hashes in 2.07406s)

--- Makefile
+++ Makefile
@@ -1,12 +1,18 @@
 CC = gcc
 CFLAGS = -O3 -Wall -ICD/Optimized_64bit --std=c99 -pthread -fno-strict-aliasing
 DEFINES = -DSKEIN_LOOP=0
+SRCS = CD/Optimized_64bit/*.c skeincrack.c
 
 all: skeincrack
 
 skeincrack: Makefile skeincrack.c
-	$(CC) $(CFLAGS) $(DEFINES) CD/Optimized_64bit/*.c skeincrack.c -o $@
+	$(CC) $(CFLAGS) $(DEFINES) $(SRCS) -o $@
+
+pgo: Makefile skeincrack.c
+	$(CC) $(CFLAGS) $(DEFINES) -fprofile-generate $(SRCS) -o skeincrack
+	./skeincrack --pgo
+	$(CC) $(CFLAGS) $(DEFINES) -fprofile-use $(SRCS) -o skeincrack
 
 .PHONY: clean
 clean:
-	rm -f *.o skeincrack
+	rm -f *.o *.gcda skeincrack
--- skeincrack.c
+++ skeincrack.c
@@ -18,7 +18,12 @@ static const int HASHES_BEFORE_BENCHMARK = 1000000;
 
 static const char *param = "hashable=";
 
-static bool benchmarkMode = false;
+static enum {
+  // We rely on BENCHMARK_MODE_OFF == 0
+  BENCHMARK_MODE_OFF = 0,
+  BENCHMARK_MODE_INTERACTIVE,
+  BENCHMARK_MODE_NON_INTERACTIVE
+} benchmarkMode;
 
 static const unsigned char target[HASH_BITS/8] = {0x5b, 0x4d, 0xa9, 0x5f, 0x5f, 0xa0, 0x82, 0x80, 0xfc, 0x98, 0x79, 0xdf, 0x44, 0xf4, 0x18, 0xc8, 0xf9, 0xf1, 0x2b, 0xa4, 0x24, 0xb7, 0x75, 0x7d, 0xe0, 0x2b, 0xbd, 0xfb, 0xae, 0x0d, 0x4c, 0x4f, 0xdf, 0x93, 0x17, 0xc8, 0x0c, 0xc5, 0xfe, 0x04, 0xc6, 0x42, 0x90, 0x73, 0x46, 0x6c, 0xf2, 0x97, 0x06, 0xb8, 0xc2, 0x59, 0x99, 0xdd, 0xd2, 0xf6, 0x54, 0x0d, 0x44, 0x75, 0xcc, 0x97, 0x7b, 0x87, 0xf4, 0x75, 0x7b, 0xe0, 0x23, 0xf1, 0x9b, 0x8f, 0x40, 0x35, 0xd7, 0x72, 0x28, 0x86, 0xb7, 0x88, 0x69, 0x82, 0x6d, 0xe9, 0x16, 0xa7, 0x9c, 0xf9, 0xc9, 0x4c, 0xc7, 0x9c, 0xd4, 0x34, 0x7d, 0x24, 0xb5, 0x67, 0xaa, 0x3e, 0x23, 0x90, 0xa5, 0x73, 0xa3, 0x73, 0xa4, 0x8a, 0x5e, 0x67, 0x66, 0x40, 0xc7, 0x9c, 0xc7, 0x01, 0x97, 0xe1, 0xc5, 0xe7, 0xf9, 0x02, 0xfb, 0x53, 0xca, 0x18, 0x58, 0xb6};
 
@@ -29,9 +34,9 @@ static const uint64_t h01 = 0x0101010101010101; //the sum of 256 to the power of
 
 int countBits(uint64_t x) {
     x -= (x >> 1) & m1;             //put count of each 2 bits into those 2 bits
-    x = (x & m2) + ((x >> 2) & m2); //put count of each 4 bits into those 4 bits 
-    x = (x + (x >> 4)) & m4;        //put count of each 8 bits into those 8 bits 
-    return (x * h01)>>56;  //returns left 8 bits of x + (x<<8) + (x<<16) + (x<<24) + ... 
+    x = (x & m2) + ((x >> 2) & m2); //put count of each 4 bits into those 4 bits
+    x = (x + (x >> 4)) & m4;        //put count of each 8 bits into those 8 bits
+    return (x * h01)>>56;  //returns left 8 bits of x + (x<<8) + (x<<16) + (x<<24) + ...
 }
 
 static inline int hammingDistance(const unsigned char *hash1,
@@ -121,7 +126,7 @@ void *hashThread(void *aux) {
     unsigned char hash[HASH_BITS / CHAR_BIT];
     Hash(HASH_BITS, buf, BUF_SIZE * CHAR_BIT, hash);
 
-    if(threadNum == 0 && benchmarkMode) {
+    if(benchmarkMode) {
       numHashes++;
       if(numHashes % HASHES_BEFORE_BENCHMARK == 0) {
         struct timeval timestamp;
@@ -132,6 +137,12 @@ void *hashThread(void *aux) {
         printf("Thread 0: %0.f hashes/sec (%d hashes in %.5fs)\n",
                numHashes / sec,
                numHashes, sec);
+
+        // Non-interactive benchmarks end at this point, while interactive
+        // benchmarks keep running until they're killed.
+        if(benchmarkMode == BENCHMARK_MODE_NON_INTERACTIVE) {
+          return NULL;
+        }
       }
     }
 
@@ -153,11 +164,6 @@ void *hashThread(void *aux) {
       best = distance;
 
       printf("Data: %s\n", buf);
-
-      // Only invoke curl if we're not in benchmark mode.
-      if (benchmarkMode) {
-        continue;
-      }
 
       int outfd = open("data.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
       if(outfd < 0)
@@ -181,7 +187,11 @@ int main(int argc, char** argv) {
   pthread_mutex_init(&bestLock, NULL);
 
   if (argc == 2 && !strcmp("--benchmark", argv[1])) {
-    benchmarkMode = true;
+    benchmarkMode = BENCHMARK_MODE_INTERACTIVE;
+  } else if (argc == 2 && !strcmp("--pgo", argv[1])) {
+    benchmarkMode = BENCHMARK_MODE_NON_INTERACTIVE;
+  } else {
+    benchmarkMode = BENCHMARK_MODE_OFF;
   }
 
   long numCPUs;
